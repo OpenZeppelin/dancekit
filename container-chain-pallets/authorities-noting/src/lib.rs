05a5bed698852571c29083a2d74fd3299bf091a1
4meta5 <asinghchrony@protonmail.com>
2025-01-16 14:01:50 -0500

    update dp consensus and pallet cc authorities noting

--- container-chain-pallets/authorities-noting/src/lib.rs
+++ container-chain-pallets/authorities-noting/src/lib.rs
@@ -28,6 +28,9 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+#[doc(hidden)]
+extern crate alloc;
+
 #[cfg(test)]
 mod mock;
 
@@ -59,7 +62,7 @@ use {
     frame_system::pallet_prelude::*,
     parity_scale_codec::{Decode, Encode},
     sp_inherents::{InherentIdentifier, IsFatalError},
-    sp_runtime::{traits::Hash as HashT, RuntimeString},
+    sp_runtime::traits::Hash as HashT,
     sp_std::prelude::*,
 };
 
@@ -279,9 +282,8 @@ pub mod pallet {
         fn is_inherent_required(_: &InherentData) -> Result<Option<Self::Error>, Self::Error> {
             // Return Ok(Some(_)) unconditionally because this inherent is required in every block
             Ok(Some(InherentError::Other(
-                sp_runtime::RuntimeString::Borrowed(
-                    "Orchestrator Authorities Noting Inherent required",
-                ),
+                alloc::borrow::Cow::Borrowed("Orchestrator Authorities Noting Inherent required")
+                    .to_string(),
             )))
         }
 
@@ -382,7 +384,7 @@ impl<T: Config> Pallet<T> {
 #[derive(Encode)]
 #[cfg_attr(feature = "std", derive(Debug, Decode))]
 pub enum InherentError {
-    Other(RuntimeString),
+    Other(alloc::string::String),
 }
 
 impl IsFatalError for InherentError {
